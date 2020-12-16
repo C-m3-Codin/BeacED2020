ccae40883d7fd8d25221c59c2b38fee826b6635e
cp <cyril199897@gmail.com>
2020-12-17 03:14:33 +0530

    Direct fix

--- contact.js
+++ contact.js
@@ -1,3 +1,19 @@
+ar __sf = $(".sponsorsFooter ul span");
+var __sf_cont = $(".sponsorsFooter ul");
+var __title = $("#sponsorTitle");
+
+__sf.mouseenter(function() {
+  $(this).addClass('hover');
+  __sf_cont.addClass('enter');
+  var __setTitle = $(this).attr("data-group-title");
+  __title.html(__setTitle);
+}).mouseleave(function() {
+  $(this).removeClass('hover');
+  __sf_cont.removeClass('enter');
+  var __setTitle = $(__title).attr("data-default-title");
+  __title.html(__setTitle);
+});
+
 $(document).ready(function() {
   $('#regisOpen').click(function() {
       $('#FunnuBone').toggle();
@@ -5,7 +21,10 @@ $(document).ready(function() {
           })
 
 
-          $('#GameSet').click(function() {
+          
+
+
+          $('#GameRegis').click(function() {
             $('#Game1').toggle();
       
                 })
